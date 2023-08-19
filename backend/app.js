d8cabeedd44840d2318a9e0640832574ebc9dd26
Aniket Arjun Khillare <95135993+aniket-017@users.noreply.github.com>
2023-08-19 08:09:31 +0530

    Update app.js 2

--- backend/app.js
+++ backend/app.js
@@ -22,10 +22,10 @@ const user = require("./routes/userRoute")
 app.use("/aak/l1",venue);
 app.use("/aak/l1",user);
 
-app.use(express.static(path.join(__dirname, "../build")));
+app.use(express.static(path.join(__dirname, "../front/build")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../build/index.html"));
+  res.sendFile(path.resolve(__dirname, "../front/build/index.html"));
 });
 
 
